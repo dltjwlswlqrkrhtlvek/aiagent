6d308667b9cd3f573dcaa19294b4d999063368e5
dltjwlswlqrkrhtlvek <gimpo.s230268@ggh.goe.go.kr>
2024-11-25 14:06:22 +0900

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ model = joblib.load('linear_regression_model (1).pkl')
 st.title('불면증 발병 위험 판독 에이전트')
 st.subheader('모델 설명')
 st.write(' - 기계학습 알고리즘 : linear_regression ')
-st.write(' - 학습 데이터 출처 : https://www.kaggle.com/')
+st.write(' - 학습 데이터 출처 : https://www.kaggle.com/code/songulerdem/idao-insomnia-prediction')
 st.write(' - 훈련    데이터 : 70,000건')
 st.write(' - 테스트 데이터 : 30,000건')
 st.write(' - 인공지능 모델 정확도 : 0.08')
@@ -31,8 +31,8 @@ a = st.selectbox('성별을 입력하세요(남자:1, 여자:2', [1,2])
 b = st.number_input(' 신장을 입력하세요.', value=0.0 )     # 초기값은 0.0
 c = st.number_input(' 몸무게를 입력하세요.', value=0.0 )
                                                             # 사용자가  0,1 중에 선
-d = st.selectbox('스트레스 정도를 입력하세요(적음:1, 보통:2,많음3', [1,3])
-e = st.selectbox('병원 방문 정도를 입력하세요(적음:1, 보통:2,많음3', [1,3])
+d = st.selectbox('스트레스 정도를 입력하세요(적음:1, 보통:2,많음3', [1,2,3])
+e = st.selectbox('병원 방문 정도를 입력하세요(적음:1, 보통:2,많음3', [1,2,3])
 f = st.selectbox('외출 정도를 입력하세요(적음:0,많음:1', [0,1])
 
 if st.button('불면증 위험도 예측'):            # 사용자가 '점수예측' 버튼을 누르면
